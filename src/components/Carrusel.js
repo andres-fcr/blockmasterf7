d447367da7f8da35b984e00929f6dcdeb49f1f57
Andres Cagua <andres.fcr089@gmail.com>
2025-04-23 20:25:27 -0500

    style: general visual updates

--- src/components/Carrusel.js
+++ src/components/Carrusel.js
@@ -1,50 +1,34 @@
-import React from "react";
-import {Button, Carousel} from "react-bootstrap";
-import {useNavigate} from "react-router-dom";
-import {Car, ImgCarousel} from "../styles/CardsStyles";
-import ModalS from "./ModalS";
+import React from 'react'
+import { Button, Carousel } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+import { Car, ImgCarousel } from '../styles/CardsStyles'
 
-const Carrusel = ({video}) => {
-	// console.log(video);
-	const navigate = useNavigate();
+const Carrusel = ({ video }) => {
+  const navigate = useNavigate()
 
-	return (
-		<Car className="mx-auto">
-			<Carousel indicators={false}>
-				{video.map((i, index) => (
-					<Carousel.Item key={index}>
-						<ImgCarousel
-							className="d-block w-100 img-fluid "
-							src={i.backdrop_path}
-							alt={i.title}
-						/>
-						<Carousel.Caption className=" bg-dark bg-opacity-75 w-25 rounded">
-							<h5 className=" bg-transparent mb-3">
-								{i.title}
-							</h5>
-							<ModalS
-								as={Button}
-								size="sm"
-								vid={i.video}
-							/>
-							<Button
-								size="sm"
-								className="mx-2"
-								variant="outline-warning"
-								onClick={() =>
-									navigate(
-										`/detalle/${i.title}`
-									)
-								}
-							>
-								Detalles
-							</Button>
-						</Carousel.Caption>
-					</Carousel.Item>
-				))}
-			</Carousel>
-		</Car>
-	);
-};
+  return (
+    <Car className="mx-auto">
+      <Carousel indicators={false}>
+        {video.map((i, index) => (
+          <Carousel.Item key={index}>
+            <ImgCarousel className="d-block w-100 img-fluid " src={i.backdrop_path} alt={i.title} />
+            <Carousel.Caption className=" bg-dark bg-opacity-75 w-lg-25 rounded" title={i.title}>
+              <h5 className=" bg-transparent mb-3 text-truncate mx-3">{i.title}</h5>
+              <p className="bg-transparent text-truncate mx-3">{i.overview}</p>
+              <Button
+                size="sm"
+                className="mx-2"
+                variant="outline-warning"
+                onClick={() => navigate(`/detalle/${i.title}`)}
+              >
+                Detalles
+              </Button>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </Car>
+  )
+}
 
-export default Carrusel;
+export default Carrusel
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,30 +7,19 @@ import { logoutAsync } from '../redux/actions/actionLogin'
 import { searchTerm } from '../redux/actions/actionSearch'
 
 const NavBar = ({ isAuthenticated, mayor, menor }) => {
-
   const dispatch = useDispatch()
 
   const handleLogout = () => {
     dispatch(logoutAsync())
   }
 
-  const [search, setSearch] = useState({
-    busqueda: ''
-  })
+  const [searchText, setSearchText] = useState('')
 
-  const { busqueda } = search
-
-  const handleInputChange = ({ target }) => {
-    setSearch({
-      ...search,
-      [target.name]: target.value
-    })
-    dispatch(searchTerm(busqueda))
-  }
-
+  const handleInputChange = ({ target }) => setSearchText(target.value)
 
   const handlesubmit = (e) => {
     e.preventDefault()
+    dispatch(searchTerm(searchText))
   }
 
   const setOrdenMayor = () => {
@@ -48,12 +37,10 @@ const NavBar = ({ isAuthenticated, mayor, menor }) => {
     menor(false)
   }
   return (
-    <Navbar style={{ padding: '10px 90px 0px 90px ' }} expand="lg" variant="dark">
+    <Navbar className="px-4" expand="lg" variant="dark">
       <Container fluid>
         <Navbar.Brand>
-          <Link
-            onClick={() => setOrdenDefault()}
-            to="/">
+          <Link onClick={() => setOrdenDefault()} to="/">
             <img
               src="https://res.cloudinary.com/dbyw7mbt6/image/upload/v1638920590/logo-blockBuster_apegqx.png"
               width="auto"
@@ -62,65 +49,81 @@ const NavBar = ({ isAuthenticated, mayor, menor }) => {
               alt="Block master"
             />
           </Link>
-
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto mx-3 my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link onClick={() => setOrdenDefault()} as={Link} to="/" style={{ color: '#FED941' }} className=" fs-5" > Todas</Nav.Link>
-            <Nav.Link onClick={() => setOrdenMayor()} style={{ color: '#FED941' }} className=" fs-5" > Mas Valoradas </Nav.Link>
-            <Nav.Link onClick={() => setOrdenMenor()} style={{ color: '#FED941' }} className=" fs-5" > Menos Valoradas</Nav.Link>
+          <Nav className="me-auto my-lg-0 mx-0" navbarScroll>
+            <Nav.Link
+              onClick={() => setOrdenDefault()}
+              as={Link}
+              to="/"
+              style={{ color: '#FED941' }}
+              className=" fs-5"
+            >
+              Todas
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => setOrdenMayor()}
+              style={{ color: '#FED941' }}
+              className=" fs-5"
+            >
+              Mas Valoradas
+            </Nav.Link>
+            <Nav.Link
+              onClick={() => setOrdenMenor()}
+              style={{ color: '#FED941' }}
+              className=" fs-5"
+            >
+              Menos Valoradas
+            </Nav.Link>
 
-            {
-              isAuthenticated &&
-              <Nav className=' position-relative end-0'>
-
-
-              </Nav>
-            }
-            {/* <Nav.Link style={{ color: '#FED941' }} onClick={() => handleLogout()}>Logout</Nav.Link> */}
-            {/* <Nav.Link as={Link} to="/new" style={{ color: '#FED941' }} >Nueva Pelicula</Nav.Link> */}
-            {/* <NavLogged isAuthenticated={isAuthenticated} /> */}
-
-
+            {isAuthenticated && <Nav className=" position-relative end-0"></Nav>}
           </Nav>
-          {
-            !isAuthenticated &&
-            <Nav className='me-3'>
-              <Nav.Link as={Link} to="/register" style={{ color: '#FED941' }} className="fs-5" >Registro</Nav.Link>
-              <Nav.Link as={Link} to="/login" style={{ color: '#FED941' }} className="fs-5" >Login</Nav.Link>
-            </Nav>
-          }
-          {
-            isAuthenticated &&
-            <Nav className='me-3'>
 
-              <NavDropdown className='text-warning fs-5' style={{ color: '#FED941' }} title="Cuenta" id="basic-nav-dropdown">
-
-                <NavDropdown.Item as={Link} to="/favoritos" className="fs-6" >Favoritos</NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/new" className="fs-6" >Nueva Pelicula </NavDropdown.Item>
-                <NavDropdown.Item onClick={() => handleLogout()} className="fs-6">Logout</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          }
-          <Form className="d-flex"
-            onSubmit={handlesubmit}
-          >
+          <Form className="d-flex" onSubmit={handlesubmit}>
             <FormControl
               variant="outline-warning"
               placeholder="Busca tu pelicula favorita"
               className="me-2 px-3"
-              name='busqueda'
-              value={busqueda}
+              name="busqueda"
+              value={searchText}
               onChange={handleInputChange}
             />
-            <Button variant="outline-warning" className="px-3"><BsSearch className='bg-transparent' /></Button>
+            <Button variant="outline-warning" className="px-3" type="submit">
+              <BsSearch className="bg-transparent" />
+            </Button>
           </Form>
+          {!isAuthenticated && (
+            <Nav className="me-3 fs-6">
+              <Nav.Link as={Link} to="/register" style={{ color: '#FED941' }} className="">
+                Registro
+              </Nav.Link>
+              <Nav.Link as={Link} to="/login" style={{ color: '#FED941' }} className="">
+                Login
+              </Nav.Link>
+            </Nav>
+          )}
+          {isAuthenticated && (
+            <Nav className="me-3">
+              <NavDropdown
+                className="text-warning fs-6"
+                style={{ color: '#FED941' }}
+                title="Cuenta"
+                id="basic-nav-dropdown"
+              >
+                <NavDropdown.Item as={Link} to="/favoritos" className="fs-6">
+                  Favoritos
+                </NavDropdown.Item>
+                <NavDropdown.Item as={Link} to="/new" className="fs-6">
+                  Nueva Pelicula
+                </NavDropdown.Item>
+                <NavDropdown.Item onClick={() => handleLogout()} className="fs-6">
+                  Logout
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
