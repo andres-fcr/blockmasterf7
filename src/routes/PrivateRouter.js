fcc5b644981ac03f7b4aa099b75d075ec4370a20
Andres Cagua <andres.fcr089@gmail.com>
2025-04-23 21:44:33 -0500

    chore: remove unused js files

--- src/routes/PrivateRouter.js
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom"
-
-export const PrivateRouter = ({ isAuthenticated, children }) => {
-    return isAuthenticated
-    ? children
-    : <Navigate to="/home"/>
-}
--- src/routes/PublicRouter.js
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom"
-
-export const PublicRoutes = ({ isAuthenticated, children }) => {
-    return !isAuthenticated
-        ? children
-        : <Navigate to="/*" />
-}
