6588ecbd30bf33423d0e6238f45d8492ee988b57
Andres Cagua <andres.fcr089@gmail.com>
2025-09-09 23:01:54 -0500

    refactor: update for navbar, mobile and desktop views

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,130 +1,143 @@
-import React, { useState } from 'react'
-import { Button, Container, Form, FormControl, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { useRef, type FormEvent } from 'react'
+import { Container, Form, InputGroup, Nav, Navbar, NavDropdown, Offcanvas } from 'react-bootstrap'
 import { BsSearch } from 'react-icons/bs'
-import { useDispatch } from 'react-redux'
-import { Link } from 'react-router-dom'
-import { logoutAsync } from '../redux/actions/actionLogin'
-import { searchTerm } from '../redux/actions/actionSearch'
+import { Link, useLocation } from 'react-router-dom'
+import { useMoviesStore } from '@/store/moviesStore'
+import { useAuthStore } from '@/store/authStore'
+import { routes } from '@/routes/constants/routes'
 
-const NavBar = ({ isAuthenticated, mayor, menor }) => {
-  const dispatch = useDispatch()
+const NavBar = ({ onAuthModal }: { onAuthModal: () => void }) => {
+  const location = useLocation()
+
+  const inputRef = useRef<HTMLInputElement>(null)
+
+  const user = useAuthStore((state) => state.user)
+
+  const setSearchTerm = useMoviesStore.use.updateSearchTerm()
+
+  const isLinkActive = (route: string) => location.pathname.startsWith(route)
 
   const handleLogout = () => {
-    dispatch(logoutAsync())
+    // dispatch(logoutAsync())
   }
 
-  const [searchText, setSearchText] = useState('')
+  const onItemSelect = (key: string | null, e: React.SyntheticEvent<unknown, Event>) => {
+    if (key === routes.register) {
+      e.preventDefault()
+      // onAuthModal()
+    }
+  }
 
-  const handleInputChange = ({ target }) => setSearchText(target.value)
-
-  const handlesubmit = (e) => {
+  const handlesubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
-    dispatch(searchTerm(searchText))
+
+    if (!inputRef.current) return
+
+    setSearchTerm(inputRef.current.value || '')
+    inputRef.current.value = ''
   }
 
-  const setOrdenMayor = () => {
-    mayor(true)
-    menor(false)
-  }
-
-  const setOrdenMenor = () => {
-    mayor(false)
-    menor(true)
-  }
-
-  const setOrdenDefault = () => {
-    mayor(false)
-    menor(false)
-  }
   return (
-    <Navbar className="px-4" expand="lg" variant="dark">
+    <Navbar
+      collapseOnSelect
+      className="px-4 border-bottom border-dark bg-body-dark"
+      expand="lg"
+      variant="dark"
+      onSelect={onItemSelect}
+    >
       <Container fluid>
         <Navbar.Brand>
-          <Link onClick={() => setOrdenDefault()} to="/">
-            <img
-              src="https://res.cloudinary.com/dbyw7mbt6/image/upload/v1638920590/logo-blockBuster_apegqx.png"
-              width="auto"
-              height="64"
-              className="d-inline-block align-top"
-              alt="Block master"
-            />
-          </Link>
+          <Nav.Link
+            as={Link}
+            to={routes.movies}
+            href={routes.movies}
+            className="text-warning link-underline-opacity-0 fs-4 pe-4"
+            style={{ textDecoration: 'none' }}
+          >
+            🎞️ BlockMaster
+          </Nav.Link>
         </Navbar.Brand>
 
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav className="me-auto my-lg-0 mx-0" navbarScroll>
-            <Nav.Link
-              onClick={() => setOrdenDefault()}
-              as={Link}
-              to="/"
-              style={{ color: '#FED941' }}
-              className=" fs-5"
-            >
-              Todas
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => setOrdenMayor()}
-              style={{ color: '#FED941' }}
-              className=" fs-5"
-            >
-              Mas Valoradas
-            </Nav.Link>
-            <Nav.Link
-              onClick={() => setOrdenMenor()}
-              style={{ color: '#FED941' }}
-              className=" fs-5"
-            >
-              Menos Valoradas
-            </Nav.Link>
+        <Navbar.Toggle aria-controls="navbarScroll" className="my-1" />
 
-            {isAuthenticated && <Nav className=" position-relative end-0"></Nav>}
-          </Nav>
+        <Navbar.Offcanvas id="navbarScroll" placement="end">
+          <Offcanvas.Header closeButton className="text-dark"></Offcanvas.Header>
 
-          <Form className="d-flex" onSubmit={handlesubmit}>
-            <FormControl
-              variant="outline-warning"
-              placeholder="Busca tu pelicula favorita"
-              className="me-2 px-3"
-              name="busqueda"
-              value={searchText}
-              onChange={handleInputChange}
-            />
-            <Button variant="outline-warning" className="px-3" type="submit">
-              <BsSearch className="bg-transparent" />
-            </Button>
-          </Form>
-          {!isAuthenticated && (
-            <Nav className="me-3 fs-6">
-              <Nav.Link as={Link} to="/register" style={{ color: '#FED941' }} className="">
-                Registro
-              </Nav.Link>
-              <Nav.Link as={Link} to="/login" style={{ color: '#FED941' }} className="">
-                Login
-              </Nav.Link>
-            </Nav>
-          )}
-          {isAuthenticated && (
-            <Nav className="me-3">
-              <NavDropdown
-                className="text-warning fs-6"
+          <Offcanvas.Body className="px-5 px-lg-0">
+            <Nav className="me-auto my-lg-0 mx-0" navbarScroll>
+              <Nav.Link
+                as={Link}
                 style={{ color: '#FED941' }}
-                title="Cuenta"
-                id="basic-nav-dropdown"
+                className={
+                  isLinkActive(routes.movies)
+                    ? 'text-decoration-underline link-offset-1 fw-medium'
+                    : ''
+                }
+                to={routes.movies}
+                href={routes.movies}
               >
-                <NavDropdown.Item as={Link} to="/favoritos" className="fs-6">
-                  Favoritos
-                </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/new" className="fs-6">
-                  Nueva Pelicula
-                </NavDropdown.Item>
-                <NavDropdown.Item onClick={() => handleLogout()} className="fs-6">
-                  Logout
-                </NavDropdown.Item>
-              </NavDropdown>
+                Peliculas
+              </Nav.Link>
+              <Nav.Link
+                as={Link}
+                style={{ color: '#FED941' }}
+                className={
+                  isLinkActive(routes.series)
+                    ? 'text-decoration-underline link-offset-1 fw-medium'
+                    : ''
+                }
+                to={routes.series}
+                href={routes.series}
+              >
+                Series
+              </Nav.Link>
             </Nav>
-          )}
-        </Navbar.Collapse>
+
+            {user && <Nav className=" position-relative end-0"></Nav>}
+
+            <Form className="d-flex px-lg-2 py-2 py-lg-0" onSubmit={handlesubmit}>
+              <InputGroup>
+                <InputGroup.Text id="search">
+                  <BsSearch className="bg-transparent" />
+                </InputGroup.Text>
+                <Form.Control
+                  ref={inputRef}
+                  aria-label="Username"
+                  aria-describedby="search"
+                  placeholder="Busca tu pelicula favorita"
+                  className="form-control-dark"
+                />
+              </InputGroup>
+            </Form>
+            {!user && (
+              <Nav className="me-3 fs-6">
+                <Nav.Link style={{ color: '#FED941' }} className="button" href={routes.register}>
+                  Acceder
+                </Nav.Link>
+              </Nav>
+            )}
+            {user && (
+              <Nav className="me-3">
+                <NavDropdown
+                  className="text-warning fs-6"
+                  style={{ color: '#FED941' }}
+                  title="Cuenta"
+                  id="basic-nav-dropdown"
+                >
+                  <NavDropdown.Item as={Link} to="/favoritos" className="fs-6">
+                    Favoritos
+                  </NavDropdown.Item>
+                  <NavDropdown.Item as={Link} to="/new" className="fs-6">
+                    Nueva Pelicula
+                  </NavDropdown.Item>
+                  <NavDropdown.Item onClick={() => handleLogout()} className="fs-6">
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            )}
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
       </Container>
     </Navbar>
   )
