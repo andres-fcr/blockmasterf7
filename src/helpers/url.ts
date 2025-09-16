bce91c95ed0b9c94ed8a5b4097ce8975e150a446
Andres Cagua <andres.fcr089@gmail.com>
2025-09-15 21:38:41 -0500

    feat: add url buiders helpers

--- src/helpers/url.ts
+++ src/helpers/url.ts
@@ -1,1 +1,42 @@
 export const url = 'https://blockmas.herokuapp.com/results'
+
+const baseImgUrl = import.meta.env.VITE_IMAGE_BASE_URL
+
+export type PosterSize = 'w92' | 'w154' | 'w185' | 'w342' | 'w500' | 'w780' | 'original'
+
+export type BackdropSize = 'w300' | 'w780' | 'w1280' | 'original'
+
+type posterParams = {
+  type: 'poster'
+  size: PosterSize
+  path: string
+}
+
+type backdropParams = {
+  type: 'backdrop'
+  size: BackdropSize
+  path: string
+}
+
+type ImgUrlParams = posterParams | backdropParams
+
+type Filters = Record<string, string | number | boolean | undefined>
+
+export function buildUrl(path: string, filters?: Filters): string {
+  const params = new URLSearchParams()
+
+  if (filters) {
+    Object.entries(filters).forEach(([key, value]) => {
+      if (value !== undefined) {
+        params.append(key, String(value))
+      }
+    })
+  }
+
+  const query = params.toString()
+  return query ? `${path}?${query}` : `${path}`
+}
+
+export const buildImageUrl = ({ path, size }: ImgUrlParams) => {
+  return `${baseImgUrl}/${size}${path}`
+}
